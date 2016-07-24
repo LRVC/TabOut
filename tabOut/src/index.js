b0af0d1d24e00f55dbb13a07e1b1f19b447ccbd0
LRVC <lrv378@gmail.com>
2016-07-23 21:13:09 -0500

    -Use npm eject script -Add components directory -Add react-router and routes for tabs, inventory, and dashboard

--- tabOut/src/index.js
+++ tabOut/src/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { Router, Route, browserHistory } from 'react-router';
+import App from './components/App';
+import Tabs from './components/Tabs';
+import Inventory from './components/Inventory';
+import Dashboard from './components/Dashboard';
 import './index.css';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+ReactDOM.render((
+    <Router history={browserHistory}>
+        <Route path="/" component={App}></Route>
+        <Route path="/tabs" component={Tabs} />
+        <Route path="/inventory" component={Inventory} />
+        <Route path="/dashboard" component={Dashboard} />
+    </Router>
+), document.getElementById('root'))
